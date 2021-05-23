0ee112846171c5487ddc1a5127bdbeadd1534a41
Lalit Pant <pant.lalit@gmail.com>
2021-05-23 13:52:03 +0530

    Tweaks.

--- src/main/java/driver/API.java
+++ src/main/java/driver/API.java
@@ -6,15 +6,27 @@ import net.kogics.kojo.lite.KojoFrame;
 import net.kogics.kojo.turtle.TurtleWorldAPI;
 
 class API {
-    static KojoFrame kojo = KojoFrame.create(true);
-    static Builtins b = kojo.builtins();
-    static TurtleWorldAPI t = b.TurtleAPI();
-    static DrawingCanvasAPI c = b.CanvasAPI();
+    static KojoFrame kojo = null;
+    static Builtins b = null;
+    static TurtleWorldAPI t = null;
+    static DrawingCanvasAPI c = null;
+    public static void createKojoFrame() {
+        kojo = KojoFrame.create();
+        initBuiltins();
+    }
+    public static void createKojoFrame(int width, int height) {
+        kojo = KojoFrame.create(width, height);
+        initBuiltins();
+    }
+    public static void initBuiltins() {
+        b = kojo.builtins();
+        t = b.TurtleAPI();
+        c = b.CanvasAPI();
+    }
 
     public static void clear() {
         c.clear();
     }
-
     public static void forward(double n) {
         t.forward(n);
     }
--- src/main/java/driver/Main4Java.java
+++ src/main/java/driver/Main4Java.java
@@ -4,6 +4,7 @@ import static driver.API.*;
 
 public class Main4Java {
     public static void main(String[] args) {
+        createKojoFrame(600, 600);
         clear();
         for (int i = 0; i < 4; i++) {
             forward(100);
