209a93ef62e0c9cd5dfb1bf514bdea210fd9080e
Lalit Pant <pant.lalit@gmail.com>
2021-05-31 22:06:01 +0530

    Bump up revision number. Tweak build.sbt

--- build.sbt
+++ build.sbt
@@ -1,6 +1,8 @@
 name := "kojo-lib"
 
-version := "0.1"
+organization := "net.kogics"
+
+version := "0.1.0"
 
 scalaVersion := "2.13.6"
 
--- src/main/scala/net/kogics/kojo/lite/Versions.scala
+++ src/main/scala/net/kogics/kojo/lite/Versions.scala
@@ -3,8 +3,8 @@ package net.kogics.kojo.lite
 object Versions {
   val KojoLibMajorVersion = "Pre 1.0"
   val KojoLibVersion = "0.1.0"
-  val KojoLibRevision = "r2"
-  val KojoLibBuildDate = "25 May 2021"
+  val KojoLibRevision = "r3"
+  val KojoLibBuildDate = "31 May 2021"
   val JavaVersion = {
     val jrv = System.getProperty("java.runtime.version")
     val arch = System.getProperty("os.arch")
