ccc3787f7db2b2c0d4dd4aaae054f99de868f6ac
bjornregnell <bjorn.regnell@cs.lth.se>
2022-04-25 17:23:43 +0200

    add localized exporters

---
+++ getting-started/kojo-english.scala
@@ -0,0 +1,10 @@
+//> using scala "3"
+//> using lib "net.kogics:kojo-lib:0.1.1,url=https://github.com/litan/kojo-lib/releases/download/v0.1.1/kojo-lib-assembly-0.1.1.jar"
+
+/* The above directives enables scala-cli to use Kojo in Scala 3 with this command:
+scala-cli repl .
+Install scala-cli from here: https://scala-cli.virtuslab.org/install  */
+
+// The line below makes English commands available on top level:
+
+export net.kogics.kojo.English.*, CanvasAPI.*, TurtleAPI.* 
