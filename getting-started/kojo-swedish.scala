2b626d9f269003d83d7f00a4769dc0264d68641b
bjornregnell <bjorn.regnell@cs.lth.se>
2022-07-04 17:19:30 +0200

    add export of builtins to getting started shims

--- getting-started/kojo-swedish.scala
+++ getting-started/kojo-swedish.scala
@@ -1,10 +1,13 @@
 //> using scala "3"
 //> using lib "net.kogics:kojo-lib:0.1.1,url=https://github.com/litan/kojo-lib/releases/download/v0.1.1/kojo-lib-assembly-0.1.1.jar"
 
-/* The above directives enables scala-cli to use Kojo in Scala 3 with this command:
+/* The above directives enable scala-cli to use Kojo in Scala 3 with this command:
 scala-cli repl .
-Install scala-cli from here: https://scala-cli.virtuslab.org/install  */
+Install scala-cli using the official installer from 
+https://www.scala-lang.org/download/
+or directly from https://scala-cli.virtuslab.org/install  */
 
-// The line below makes Swedish and English commands available on top level:
+// The lines below make Swedish and English commands available on top level:
 
 export net.kogics.kojo.Swedish.*, padda.*, CanvasAPI.*, TurtleAPI.*
+export builtins.{TurtleAPI => _, CanvasAPI => _, *}
