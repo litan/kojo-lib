3343c7334bb546c0baf9ae179306112acde9850b
Lalit Pant <pant.lalit@gmail.com>
2025-07-03 19:39:25 +0530

    Apply fix provided by Bjorn.

--- getting-started/kojo-swedish.scala
+++ getting-started/kojo-swedish.scala
@@ -1,12 +1,13 @@
-//> using scala "3"
-//> using lib "net.kogics:kojo-lib:0.3.1,url=https://github.com/litan/kojo-lib/releases/download/v0.1.0/kojo-lib-assembly-0.3.1.jar"
+//> using scala 3
+//> using dep "net.kogics:kojo-lib:0.3.1,url=https://github.com/litan/kojo-lib/releases/download/v0.3.1/kojo-lib-assembly-0.3.1.jar"
 
-/* The above directives enable scala-cli to use Kojo in Scala 3 with this command:
-scala-cli repl .
-Install scala-cli using the official installer from 
+/* The above directives enable scala repl to use Kojo in Scala 3 with this command:
+scala repl .
+Install scala using the official installer from
 https://www.scala-lang.org/download/
-or directly from https://scala-cli.virtuslab.org/install  */
+*/
 
 // The lines below make Swedish and English commands available on top level:
 
 export net.kogics.kojo.Swedish.*, padda.*, CanvasAPI.*, TurtleAPI.*
+export java.awt.Color
